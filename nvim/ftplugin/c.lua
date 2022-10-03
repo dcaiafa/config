5e261e8eb5ddae3c5f708ea907e6b23c3d6e42dd
Daniel Caiafa <dcaiafa@istreamplanet.com>
2022-10-03 08:11:14 -0700

    changes

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -1,6 +1,3 @@
 require("which-key").register({
-  c = {
-    name = "Coding",
-    h = { "<cmd>ClangdSwitchSourceHeader<cr>", "Switch between header/implementation" },
-  }
-}, { prefix = "<leader>", mode = "n", { silent = true } })
+  h = { "<cmd>ClangdSwitchSourceHeader<cr>", "Switch between header/implementation" }
+}, { prefix = "g", mode = "n", { silent = true } })
