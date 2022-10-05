68d2e2e9b69feaf33728323aa687b49120e3e0a3
Daniel Caiafa <dcaiafa@istreamplanet.com>
2022-10-05 10:12:48 -0700

    Fix init.lua for neovim 0.7.8

--- nvim/lua/my-config/init.lua
+++ nvim/lua/my-config/init.lua
@@ -3,9 +3,3 @@ require("my-config.options")
 require("my-config.mappings")
 require("my-config.commands")
 require("my-config.autocmd")
-
--- Enable filetype.lua.
--- The feature is currently opt-in.
-vim.g.do_filetype_lua = 1
--- disable filetype.vim
-vim.g.did_load_filetypes = 0
