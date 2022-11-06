a92eb9271f8dbd45cf8e47ce2969849c05e79315
Daniel Caiafa <dcaiafa@istreamplanet.com>
2022-11-06 10:29:57 -0800

    Fix terminal size-update on buffer switch

--- nvim/lua/my-config/functions.lua
+++ nvim/lua/my-config/functions.lua
@@ -8,15 +8,17 @@ M.open_terminal = function(n)
   if fn.bufexists(buf) ~= 1 then
     cmd('terminal')
     cmd('file ' .. buf)
-    if fn.mode() == 'n' then
-      cmd('startinsert')
-    end
+    cmd('startinsert')
     return
   end
+
+  -- Switch to buffer and enter insert mode.
+  -- There seems to be a bug where sometimes the terminal size is not updated
+  -- correctly. Re-entering insert mode fixes this problem, as long as it is
+  -- done in a timer.
   cmd('buffer ' .. buf)
-  if fn.mode() == 'n' then
-    cmd('startinsert')
-  end
+  fn.timer_start(100, function() vim.cmd('stopinsert') end)
+  fn.timer_start(200, function() vim.cmd('startinsert') end)
 end
 
 return M
