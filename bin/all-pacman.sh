8f6c29cd2fa8a9fc3712fb0312b442eeeeb7c896
Daniel Caiafa <dcaiafa@istreamplanet.com>
2024-05-18 13:36:22 -0700

    all-pacman.sh

---
+++ bin/all-pacman.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+sudo pacman -S --needed \
+  arandr aribb24 base base-devel \
+  bluez bluez-utils clang cmake \
+  delve docker docker-buildx dolphin \
+  dosfstools dunst efibootmgr expac \
+  feh font-manager fzf git \
+  github-cli gnome-screenshot gnuplot go \
+  go-yq godot gopls graphviz \
+  helm inetutils jq kcharselect \
+  kcolorchooser kitty krita kubectl \
+  less libreoffice-still lightdm lightdm-gtk-greeter \
+  lightdm-gtk-greeter-settings linux linux-firmware linux-headers \
+  lld mupdf neovim net-tools \
+  networkmanager ninja nodejs noto-fonts \
+  noto-fonts-cjk noto-fonts-emoji noto-fonts-extra npm \
+  nvidia nvidia-settings pavucontrol picom \
+  pipewire-audio pipewire-pulse plasma-browser-integration pritunl-client-electron \
+  python-dbus-next python-pip python-psutil python-pynvim \
+  python-setproctitle qtile rclone rofi \
+  rust-analyzer rustup speedcrunch sudo \
+  tcpdump terraform ttf-firacode-nerd unzip \
+  vdpauinfo vlc which xclip \
+  xdg-desktop-portal-gtk zoxide zsh \
